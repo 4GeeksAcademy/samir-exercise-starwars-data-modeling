e1a15416f26495fec8e6ce1c3310a72495a7ddd9
Samir Mondabla <163849611+RomiBeak@users.noreply.github.com>
2024-07-19 12:01:01 +0000

    finnish

--- src/models.py
+++ src/models.py
@@ -7,23 +7,55 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+    userID = Column(Integer, primary_key=True)
+    Email = Column(String)
+    Password = Column(String)
+    First_Name = Column(String)
+    Last_Name = Column(String)
+    Favorites = relationship("favorites", secondary="user_favorites", backref="user")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Planet(Base):
+    __tablename__ = 'Planet'
+    PlanetID = Column(Integer, primary_key=True)
+    Name = Column(String)
+    Description = Column(String)
+    Image = Column(String)
+    Favorites = relationship("favorites", backref="Planet")
+
+class Character(Base):
+    __tablename__ = 'character'
+    characterID = Column(Integer, primary_key=True)
+    Name = Column(String)
+    Description = Column(String)
+    Image = Column(String)
+    Favorites = relationship("favorites", backref="character")
+
+class UserJedi(Base):
+    __tablename__ = 'user_jedi'
+    userJediID = Column(Integer, primary_key=True)
+    characterID = Column(Integer, ForeignKey('character.characterID'))
+    character = relationship("Character", backref="user_jedi")
+
+class UserSith(Base):
+    __tablename__ = 'user_sith'
+    userSithID = Column(Integer, primary_key=True)
+    characterID = Column(Integer, ForeignKey('character.characterID'))
+    character = relationship("Character", backref="user_sith")
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    favoritesID = Column(Integer, primary_key=True)
+    userID = Column(Integer, ForeignKey('user.userID'))
+    PlanetID = Column(Integer, ForeignKey('Planet.PlanetID'))
+    FharacterID = Column(Integer, ForeignKey('character.characterID'))
+    Users = relationship("user", secondary="user_favorites", backref="favorites")
+
+class Userfavorites(Base):
+    __tablename__ = 'user_favorites'
+    userID = Column(Integer, ForeignKey('user.userID'), primary_key=True)
+    FavoritesID = Column(Integer, ForeignKey('favorites.favoritesID'), primary_key=True)
 
     def to_dict(self):
         return {}
